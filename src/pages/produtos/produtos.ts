50be8c7c1bee4ca5b92ae29f40c8bfbbc5d6454b
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-12-23 22:43:44 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
-import { API_CONFIG } from '../../config/api.config';]import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
 @Component({
@@ -10,7 +10,7 @@ import { API_CONFIG } from '../../config/api.config';]import { ProdutoService } 
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items: ProdutoDTO[];
 
   constructor(
     public navCtrl: NavController,
@@ -23,5 +23,19 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
-  };
+        this.loadImageUrls();
+      },
+      error => {});
+  }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
+  }  
 }
